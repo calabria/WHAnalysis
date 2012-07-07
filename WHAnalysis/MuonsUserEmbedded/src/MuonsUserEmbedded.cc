7dc9ee8449c0fdf22656c9e81a2f55f2fe7019bc
Cesare <cesare.calabria23@gmail.com>
2012-07-07 18:42:51 +0200

    Update for MVA Id/Iso

--- WHAnalysis/MuonsUserEmbedded/src/MuonsUserEmbedded.cc
+++ WHAnalysis/MuonsUserEmbedded/src/MuonsUserEmbedded.cc
@@ -34,11 +34,23 @@
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
 #include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/Muon.h"
 
 #include "DataFormats/RecoCandidate/interface/IsoDepositVetos.h"
 #include "DataFormats/RecoCandidate/interface/IsoDeposit.h"
 #include "DataFormats/PatCandidates/interface/Isolation.h"
 
+#include "Muon/MuonAnalysisTools/interface/MuonMVAEstimator.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
+#include "DataFormats/PatCandidates/interface/Electron.h"
+#include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
+#include "TrackingTools/TransientTrack/plugins/TransientTrackBuilderESProducer.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
+#include "DataFormats/GeometryVector/interface/GlobalPoint.h"
+#include "DataFormats/GeometryVector/interface/VectorUtil.h"
 
 //
 // class declaration
@@ -65,6 +77,18 @@ class MuonsUserEmbedded : public edm::EDProducer {
 
      edm::InputTag muonTag_;
      edm::InputTag vertexTag_;
+
+     bool doMuIdMVA_;
+     bool doMuIsoMVA_;
+     bool doMuIsoRingsRadMVA_;
+     bool doMuIdIsoCombMVA_;
+     std::string target;
+     MuonEffectiveArea::MuonEffectiveAreaTarget target_;
+
+     MuonMVAEstimator* fMuonIDMVA_;
+     MuonMVAEstimator* fMuonIsoMVA_;
+     MuonMVAEstimator* fMuonIsoRingsRadMVA_;
+     MuonMVAEstimator* fMuonIDIsoCombinedMVA_;
 
      reco::isodeposit::AbsVetos vetos2010Charged_;
      reco::isodeposit::AbsVetos vetos2010Neutral_;  
@@ -95,20 +119,106 @@ class MuonsUserEmbedded : public edm::EDProducer {
 //
 MuonsUserEmbedded::MuonsUserEmbedded(const edm::ParameterSet& iConfig)
 {
-   //register your products
-/* Examples
-   produces<ExampleData2>();
-
-   //if do put with a label
-   produces<ExampleData2>("label");
- 
-   //if you want to put into the Run
-   produces<ExampleData2,InRun>();
-*/
-   //now do what ever other initialization is needed
 
   muonTag_ = iConfig.getParameter<edm::InputTag>("muonTag");
   vertexTag_ = iConfig.getParameter<edm::InputTag>("vertexTag");
+
+  doMuIdMVA_ = iConfig.getParameter<bool>("doMuIdMVA");
+  doMuIsoMVA_ = iConfig.getParameter<bool>("doMuIsoMVA");
+  doMuIsoRingsRadMVA_ = iConfig.getParameter<bool>("doMuIsoRingsRadMVA");
+  doMuIdIsoCombMVA_ = iConfig.getParameter<bool>("doMuIdIsoCombMVA");
+  target = iConfig.getParameter<std::string>("target");
+
+  if( doMuIdMVA_ ){
+
+   fMuonIDMVA_ = new MuonMVAEstimator();
+   edm::FileInPath inputFileName0 = iConfig.getParameter<edm::FileInPath>("inputFileName0");
+   edm::FileInPath inputFileName1 = iConfig.getParameter<edm::FileInPath>("inputFileName1");
+   edm::FileInPath inputFileName2 = iConfig.getParameter<edm::FileInPath>("inputFileName2");
+   edm::FileInPath inputFileName3 = iConfig.getParameter<edm::FileInPath>("inputFileName3");
+   edm::FileInPath inputFileName4 = iConfig.getParameter<edm::FileInPath>("inputFileName4");
+   edm::FileInPath inputFileName5 = iConfig.getParameter<edm::FileInPath>("inputFileName5");   
+
+   vector<string> muonid_weightfiles;
+   muonid_weightfiles.push_back(inputFileName0.fullPath().data());
+   muonid_weightfiles.push_back(inputFileName1.fullPath().data());
+   muonid_weightfiles.push_back(inputFileName2.fullPath().data());
+   muonid_weightfiles.push_back(inputFileName3.fullPath().data());
+   muonid_weightfiles.push_back(inputFileName4.fullPath().data());
+   muonid_weightfiles.push_back(inputFileName5.fullPath().data());  
+
+   fMuonIDMVA_->initialize("MuonID_BDTG",
+                           MuonMVAEstimator::kID,
+                           kTRUE,
+                           muonid_weightfiles); 
+  }
+
+  if( doMuIsoMVA_ ){
+
+   fMuonIsoMVA_ = new MuonMVAEstimator();
+   edm::FileInPath inputFileName0v2 = iConfig.getParameter<edm::FileInPath>("inputFileName0v2");
+   edm::FileInPath inputFileName1v2 = iConfig.getParameter<edm::FileInPath>("inputFileName1v2");
+   edm::FileInPath inputFileName2v2 = iConfig.getParameter<edm::FileInPath>("inputFileName2v2");
+   edm::FileInPath inputFileName3v2 = iConfig.getParameter<edm::FileInPath>("inputFileName3v2");
+   edm::FileInPath inputFileName4v2 = iConfig.getParameter<edm::FileInPath>("inputFileName4v2");
+   edm::FileInPath inputFileName5v2 = iConfig.getParameter<edm::FileInPath>("inputFileName5v2");   
+
+   vector<string> muoniso_weightfiles;
+   muoniso_weightfiles.push_back(inputFileName0v2.fullPath().data());
+   muoniso_weightfiles.push_back(inputFileName1v2.fullPath().data());
+   muoniso_weightfiles.push_back(inputFileName2v2.fullPath().data());
+   muoniso_weightfiles.push_back(inputFileName3v2.fullPath().data());
+   muoniso_weightfiles.push_back(inputFileName4v2.fullPath().data());
+   muoniso_weightfiles.push_back(inputFileName5v2.fullPath().data());   
+   fMuonIsoMVA_->initialize("MuonIso_BDTG_IsoRings",
+	    		    MuonMVAEstimator::kIsoRings,
+ 	    		    kTRUE,
+ 	    		    muoniso_weightfiles);
+  }
+
+  if( doMuIsoRingsRadMVA_ ){
+
+   fMuonIsoRingsRadMVA_ = new MuonMVAEstimator();
+   edm::FileInPath inputFileName0v3 = iConfig.getParameter<edm::FileInPath>("inputFileName0v3");
+   edm::FileInPath inputFileName1v3 = iConfig.getParameter<edm::FileInPath>("inputFileName1v3");
+   edm::FileInPath inputFileName2v3 = iConfig.getParameter<edm::FileInPath>("inputFileName2v3");
+   edm::FileInPath inputFileName3v3 = iConfig.getParameter<edm::FileInPath>("inputFileName3v3");
+   edm::FileInPath inputFileName4v3 = iConfig.getParameter<edm::FileInPath>("inputFileName4v3");
+   edm::FileInPath inputFileName5v3 = iConfig.getParameter<edm::FileInPath>("inputFileName5v3");   
+
+   vector<string> muonisoRingsRad_weightfiles;
+   muonisoRingsRad_weightfiles.push_back(inputFileName0v3.fullPath().data());
+   muonisoRingsRad_weightfiles.push_back(inputFileName1v3.fullPath().data());
+   muonisoRingsRad_weightfiles.push_back(inputFileName2v3.fullPath().data());
+   muonisoRingsRad_weightfiles.push_back(inputFileName3v3.fullPath().data());
+   muonisoRingsRad_weightfiles.push_back(inputFileName4v3.fullPath().data());
+   muonisoRingsRad_weightfiles.push_back(inputFileName5v3.fullPath().data());   
+   fMuonIsoRingsRadMVA_->initialize("MuonIso_BDTG_IsoRingsRad",
+                                    MuonMVAEstimator::kIsoRingsRadial,
+                                    kTRUE,
+                                    muonisoRingsRad_weightfiles);
+  }
+
+  if( doMuIdIsoCombMVA_ ){
+
+   fMuonIDIsoCombinedMVA_ = new MuonMVAEstimator();
+   edm::FileInPath inputFileName0v4 = iConfig.getParameter<edm::FileInPath>("inputFileName0v4");
+   edm::FileInPath inputFileName1v4 = iConfig.getParameter<edm::FileInPath>("inputFileName1v4");
+   edm::FileInPath inputFileName2v4 = iConfig.getParameter<edm::FileInPath>("inputFileName2v4");
+   edm::FileInPath inputFileName3v4 = iConfig.getParameter<edm::FileInPath>("inputFileName3v4");
+   edm::FileInPath inputFileName4v4 = iConfig.getParameter<edm::FileInPath>("inputFileName4v4");  
+
+   vector<string> muonidiso_weightfiles;
+   muonidiso_weightfiles.push_back(inputFileName0v4.fullPath().data());
+   muonidiso_weightfiles.push_back(inputFileName1v4.fullPath().data());
+   muonidiso_weightfiles.push_back(inputFileName2v4.fullPath().data());
+   muonidiso_weightfiles.push_back(inputFileName3v4.fullPath().data());
+   muonidiso_weightfiles.push_back(inputFileName4v4.fullPath().data());   
+   fMuonIDIsoCombinedMVA_->initialize("MuonIso_BDTG_IsoRings",
+                                      MuonMVAEstimator::kIDIsoRingsCombined,
+                                      kTRUE,
+                                      muonidiso_weightfiles);
+  }
  
   produces<pat::MuonCollection>("");
   
@@ -118,8 +228,10 @@ MuonsUserEmbedded::MuonsUserEmbedded(const edm::ParameterSet& iConfig)
 MuonsUserEmbedded::~MuonsUserEmbedded()
 {
  
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
+   if(doMuIdMVA_) delete fMuonIDMVA_;
+   if(doMuIsoMVA_) delete fMuonIsoMVA_;
+   if(doMuIsoRingsRadMVA_) delete fMuonIsoRingsRadMVA_;
+   if(doMuIdIsoCombMVA_) delete fMuonIDIsoCombinedMVA_;
 
 }
 
@@ -132,36 +244,134 @@ MuonsUserEmbedded::~MuonsUserEmbedded()
 void
 MuonsUserEmbedded::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
-/* This is an event example
-   //Read 'ExampleData' from the Event
-   Handle<ExampleData> pIn;
-   iEvent.getByLabel("example",pIn);
-
-   //Use the ExampleData to create an ExampleData2 which 
-   // is put into the Event
-   std::auto_ptr<ExampleData2> pOut(new ExampleData2(*pIn));
-   iEvent.put(pOut);
-*/
-
-/* this is an EventSetup example
-   //Read SetupData from the SetupRecord in the EventSetup
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-*/
+  using namespace edm;
+  using namespace reco;
 
   edm::Handle<pat::MuonCollection> muonsHandle;
   iEvent.getByLabel(muonTag_,muonsHandle);
   const pat::MuonCollection* muons = muonsHandle.product();
 
+  edm::Handle<reco::MuonCollection> recoMuonsHandle;
+  iEvent.getByLabel("muons",recoMuonsHandle);
+  const reco::MuonCollection* recoMuons = recoMuonsHandle.product();
+
   edm::Handle<reco::VertexCollection> vertexHandle;
   iEvent.getByLabel(vertexTag_,vertexHandle);
   const reco::VertexCollection* vertexes = vertexHandle.product();
+
+  edm::Handle<reco::PFCandidateCollection> pfHandle;
+  iEvent.getByLabel("particleFlow",pfHandle);
+  if( !pfHandle.isValid() )  
+    edm::LogError("DataNotAvailable")
+      << "No pf particles label available \n";
+  const reco::PFCandidateCollection* pfCandidates = pfHandle.product();
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  edm::Handle<reco::VertexCollection> hVertex;
+  iEvent.getByLabel("offlinePrimaryVertices", hVertex);
+  const reco::VertexCollection *pvCol = hVertex.product();
+
+  Handle<double> hRho;
+  edm::InputTag tag("kt6PFJets","rho");
+  iEvent.getByLabel(tag,hRho);
+  double Rho = *hRho;
+
+  Handle<reco::PFCandidateCollection> hPfCandProduct;
+  iEvent.getByLabel("particleFlow", hPfCandProduct);
+  const reco::PFCandidateCollection &inPfCands = *(hPfCandProduct.product());
+
+  reco::MuonCollection IdentifiedMuons;
+
+  InputTag gsfEleLabel(string("gsfElectrons"));
+  Handle<reco::GsfElectronCollection> theEGammaCollection;
+  iEvent.getByLabel(gsfEleLabel,theEGammaCollection);
+  const reco::GsfElectronCollection inElectrons = *(theEGammaCollection.product());
+
+  reco::GsfElectronCollection IdentifiedElectrons;
+
+  for (reco::GsfElectronCollection::const_iterator iE = inElectrons.begin(); 
+       iE != inElectrons.end(); ++iE) {
+
+    double electronTrackZ = 0;
+    if (iE->gsfTrack().isNonnull()) {
+      electronTrackZ = iE->gsfTrack()->dz(pvCol->at(0).position());
+    } else if (iE->closestCtfTrackRef().isNonnull()) {
+      electronTrackZ = iE->closestCtfTrackRef()->dz(pvCol->at(0).position());
+    }    
+    if(fabs(electronTrackZ) > 0.2)  continue;
+
+    
+    if(fabs(iE->superCluster()->eta())<1.479) {     
+      if(iE->pt() > 20) {
+        if(iE->sigmaIetaIeta()       > 0.01)  continue;
+        if(fabs(iE->deltaEtaSuperClusterTrackAtVtx()) > 0.007) continue;
+        if(fabs(iE->deltaPhiSuperClusterTrackAtVtx()) > 0.8)  continue;
+        if(iE->hadronicOverEm()       > 0.15)  continue;    
+      } else {
+        if(iE->sigmaIetaIeta()       > 0.012)  continue;
+        if(fabs(iE->deltaEtaSuperClusterTrackAtVtx()) > 0.007) continue;
+        if(fabs(iE->deltaPhiSuperClusterTrackAtVtx()) > 0.8)  continue;
+        if(iE->hadronicOverEm()       > 0.15) continue;    
+      } 
+    } else {     
+      if(iE->pt() > 20) {
+        if(iE->sigmaIetaIeta()       > 0.03)  continue;
+        if(fabs(iE->deltaEtaSuperClusterTrackAtVtx()) > 0.010) continue;
+        if(fabs(iE->deltaPhiSuperClusterTrackAtVtx()) > 0.8)  continue;
+      } else {
+        if(iE->sigmaIetaIeta()       > 0.032)  continue;
+        if(fabs(iE->deltaEtaSuperClusterTrackAtVtx()) > 0.010) continue;
+        if(fabs(iE->deltaPhiSuperClusterTrackAtVtx()) > 0.8)  continue;
+      }
+    }
+    IdentifiedElectrons.push_back(*iE);
+  }
+
+  if(target == "2011Data") {
+    	target_ = MuonEffectiveArea::kMuEAData2011;
+  } else if(target == "2012Data") {
+    	target_ = MuonEffectiveArea::kMuEAData2012;
+  } else if(target == "Fall11MC") {
+    	target_ = MuonEffectiveArea::kMuEAFall11MC;
+  } else if(target == "Summer11MC") {
+    	target_ = MuonEffectiveArea::kMuEASummer11MC;
+  } else{
+	 throw cms::Exception("UnknownTarget")
+	 << "Bad eff. area option for muons: " << target
+	 << " options are: 2011Data, 2012Data, Fall11MC, Summer11MC" << std::endl;
+  }
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   std::auto_ptr< pat::MuonCollection > muonsUserEmbeddedColl( new pat::MuonCollection() ) ;
 
   for(unsigned int i = 0; i < muons->size(); i++){
     pat::Muon aMuon( (*muons)[i] );
+
+    const reco::Muon* aRecoMuon = 0;
+    for(unsigned int j = 0; j < recoMuons->size(); j++){
+      if( Geom::deltaR( (*recoMuons)[j].p4() , aMuon.p4()) < 1e-03 ) { 
+	aRecoMuon = &((*recoMuons)[j]);
+	//std::cout << "Match to recoMuon" << std::endl;
+      }
+    }
+
+    //std::cout << "@@@@@@@@ recoMuon: " << aMuon.px() << ", " << aMuon.py() << ", " << aMuon.pz() << std::endl;
+    int isPFMuon = 0;
+    for(unsigned int j = 0; j < pfCandidates->size(); j++){
+      if( (*pfCandidates)[j].particleId() == reco::PFCandidate::mu ) { 
+	reco::MuonRef muonRefToPFMuon = (*pfCandidates)[j].muonRef();
+	//if( muonRefToPFMuon.isNonnull() ){
+	//std::cout << j << ": muonRefToPFMuon: " << muonRefToPFMuon->px() << ", " 
+	//    << muonRefToPFMuon->py() << ", " << muonRefToPFMuon->pz() << std::endl;
+	//}
+	if( muonRefToPFMuon.isNonnull() && 
+	    Geom::deltaR( muonRefToPFMuon->p4() , aMuon.p4()) < 1e-04 &&
+	    (muonRefToPFMuon->isGlobalMuon() || muonRefToPFMuon->isTrackerMuon() ) )
+	  isPFMuon = 1;
+      }
+    }
    
     double dxyWrtPV =  -99.;
     double dzWrtPV  =  -99.;
@@ -177,17 +387,18 @@ MuonsUserEmbedded::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 
     aMuon.addUserFloat("dxyWrtPV",dxyWrtPV);
     aMuon.addUserFloat("dzWrtPV",dzWrtPV);
+    aMuon.addUserInt("isPFMuon",isPFMuon);
 
-    int isGlobalMuon_ = 1;
-    int isTrackerMuon_ = 1;
-    int globalMuonPromptTight_ = 1;
-    int allArbitrated_ = 1;
-    double normalizeChi2_ = -99;
-    double ptError_ = -99;
-    int innerTrackHitPattern_ = 99;
-    int numberOfValidPixelHits_ = 99;
-    int numMuonStations_ = 99;
-    int numberOfMatches_ = 99;
+    int isGlobalMuon_ = 0;
+    int isTrackerMuon_ = 0;
+    int globalMuonPromptTight_ = 0;
+    int allArbitrated_ = 0;
+    double normalizeChi2_ = 99;
+    double ptError_ = 99;
+    int innerTrackHitPattern_ = -99;
+    int numberOfValidPixelHits_ = -99;
+    int numMuonStations_ = -99;
+    int numberOfMatches_ = -99;
 
     isGlobalMuon_ = aMuon.isGlobalMuon();
     isTrackerMuon_ = aMuon.isTrackerMuon();
@@ -200,26 +411,24 @@ MuonsUserEmbedded::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 	globalMuonPromptTight_ = muon::isGoodMuon(aMuon, muon::GlobalMuonPromptTight);
 	allArbitrated_ =  muon::isGoodMuon(aMuon, muon::AllArbitrated);
 
-	reco::TrackRef muonTrack;
-	if (isValidRef(muonTrack)) {
+	normalizeChi2_ = aMuon.globalTrack()->normalizedChi2();
+	ptError_ = (aMuon.innerTrack()->ptError())/(aMuon.innerTrack()->pt()); 
 
-		normalizeChi2_ = aMuon.globalTrack()->normalizedChi2();
-		ptError_ = (aMuon.innerTrack()->ptError())/(aMuon.innerTrack()->pt()); 
+	const reco::HitPattern& innerTrackHitPattern = aMuon.innerTrack()->hitPattern();
+	innerTrackHitPattern_ = innerTrackHitPattern.numberOfValidTrackerHits();
+	numberOfValidPixelHits_ = innerTrackHitPattern.numberOfValidPixelHits();
 
-		const reco::HitPattern& innerTrackHitPattern = aMuon.innerTrack()->hitPattern();
-		innerTrackHitPattern_ = innerTrackHitPattern.numberOfValidTrackerHits();
-		numberOfValidPixelHits_ = innerTrackHitPattern.numberOfValidPixelHits();
+	int numMuonStations = 0;
+	unsigned int theStationMask = (unsigned int)aMuon.stationMask(reco::Muon::SegmentAndTrackArbitration);
+	for( int i = 0; i < 8; ++i ){ // eight stations, eight bits
 
-	      	int numMuonStations = 0;
-		unsigned int theStationMask = (unsigned int)aMuon.stationMask(reco::Muon::SegmentAndTrackArbitration);
-		for( int i = 0; i < 8; ++i ){ // eight stations, eight bits
 			if ( theStationMask & (1<<i) ) ++numMuonStations;
-		}
-
-		numMuonStations_ = numMuonStations;
-		numberOfMatches_ = aMuon.numberOfMatches();
 
 	}
+
+	numMuonStations_ = numMuonStations;
+	numberOfMatches_ = aMuon.numberOfMatches();
+
     }
 
     int muonID = isGlobalMuon_ && isTrackerMuon_ && globalMuonPromptTight_ && allArbitrated_ && (fabs(dxyWrtPV) < 0.02) && (fabs(dzWrtPV) < 0.2) && (normalizeChi2_ < 10) && (ptError_ < 0.1) && (innerTrackHitPattern_ >= 10) && (numberOfValidPixelHits_ >= 1) && (numMuonStations_ >= 2) && (numberOfMatches_ >= 1);
@@ -239,6 +448,50 @@ MuonsUserEmbedded::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
     aMuon.addUserInt("numMuonStations",numMuonStations_);
     aMuon.addUserInt("numberOfMatches",numberOfMatches_);
     aMuon.addUserInt("muonID",muonID);
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    float idmva = -99;
+    float isomva = -99;
+    float isoringsradmva = -99; 
+    float idisomva = -99;
+
+    if(doMuIdMVA_){
+
+	    idmva = fMuonIDMVA_->mvaValue( aMuon, pvCol->at(0), 
+					inPfCands, Rho, 
+		                    	target_, 
+					IdentifiedElectrons, IdentifiedMuons);}
+
+    if(doMuIsoMVA_){
+
+	    isomva = fMuonIsoMVA_->mvaValue( aMuon, pvCol->at(0), 
+					inPfCands, Rho, 
+					target_, 
+					IdentifiedElectrons, IdentifiedMuons);}
+
+    if(doMuIsoRingsRadMVA_){
+
+	    isoringsradmva = fMuonIsoRingsRadMVA_->mvaValue( aMuon, pvCol->at(0), 
+					inPfCands, Rho, 
+					target_, 
+					IdentifiedElectrons, IdentifiedMuons);}
+
+    if(doMuIdIsoCombMVA_){
+
+	    idisomva = fMuonIDIsoCombinedMVA_->mvaValue( aMuon, pvCol->at(0), 
+					inPfCands, Rho, 
+					target_, 
+					IdentifiedElectrons, IdentifiedMuons);}
+
+    //std::cout<<"MVA "<<idmva<<" "<<isomva<<" "<<isoringsradmva<<" "<<idisomva<<std::endl;
+
+    aMuon.addUserFloat("muonIdMVA",idmva);
+    aMuon.addUserFloat("muonIsoRingsMVA",isomva);
+    aMuon.addUserFloat("muonIsoRingsRadMVA",isoringsradmva);
+    aMuon.addUserFloat("muonIdIsoCombMVA",idisomva);
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     // iso deposits
     reco::isodeposit::AbsVetos vetos2010Charged;
@@ -339,10 +592,9 @@ MuonsUserEmbedded::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 
   }
 
-
   iEvent.put( muonsUserEmbeddedColl );
   return;
- 
+
 }
 
 // ------------ method called once each job just before starting event loop  ------------
