8a98de672f0b795f721dbfab2d735aaa5c0324f3
Cesare <cesare.calabria23@gmail.com>
2012-07-11 09:17:01 +0200

    better visualization

--- WHAnalysis/Configuration/test/corruptedFiles.sh
+++ WHAnalysis/Configuration/test/corruptedFiles.sh
@@ -1,61 +1,53 @@
 #!/bin/bash
 
-rm out.txt
+#rm out.txt
 
-for folder1 in `ls /lustre/cms/store/data/Run2011A/SingleMu/AOD/05Aug2011-v1/`; do
-	for folder2 in `ls /lustre/cms/store/data/Run2011A/SingleMu/AOD/05Aug2011-v1/${folder1}/`; do
+for folder1 in `ls /lustre/cms/store/data/Run2011A/DoubleElectron/AOD/05Aug2011-v1/`; do
+	#for folder2 in `ls /lustre/cms/store/data/Run2011A/DoubleElectron/AOD/05Aug2011-v1/${folder1}/`; do
+		for file in `ls -l /lustre/cms/store/data/Run2011A/DoubleElectron/AOD/05Aug2011-v1/${folder1}/ | grep ' ? ' | awk '{print $7}' `; do
 
 		#echo $folder1 $folder2
-		ls -l /lustre/cms/store/data/Run2011A/SingleMu/AOD/05Aug2011-v1/${folder1}/${folder2} | grep ' ? ' #>> out.txt
+		echo /lustre/cms/store/data/Run2011A/DoubleElectron/AOD/05Aug2011-v1/${folder1}/${file}
+		done
+	#done
+done
 
+for folder1 in `ls /lustre/cms/store/data/Run2011A/DoubleElectron/AOD/May10ReReco-v1/`; do
+	#for folder2 in `ls /lustre/cms/store/data/Run2011A/DoubleElectron/AOD/May10ReReco-v1/${folder1}/`; do
+		for file in `ls -l /lustre/cms/store/data/Run2011A/DoubleElectron/AOD/May10ReReco-v1/${folder1}/ | grep ' ? ' | awk '{print $7}' `; do
+
+		#echo $folder1 $folder2
+		echo /lustre/cms/store/data/Run2011A/DoubleElectron/AOD/May10ReReco-v1/${folder1}/${file}
+		done
+	#done
+done
+
+for folder1 in `ls /lustre/cms/store/data/Run2011A/DoubleElectron/AOD/PromptReco-v4/000/`; do
+	for folder2 in `ls /lustre/cms/store/data/Run2011A/DoubleElectron/AOD/PromptReco-v4/000/${folder1}/`; do
+		for file in `ls -l /lustre/cms/store/data/Run2011A/DoubleElectron/AOD/PromptReco-v4/000/${folder1}/${folder2}/ | grep ' ? ' | awk '{print $7}' `; do
+
+		#echo $folder1 $folder2
+		echo /lustre/cms/store/data/Run2011A/DoubleElectron/AOD/PromptReco-v4/000/${folder1}/${folder2}/${file}
+		done
 	done
 done
 
-for folder1 in `ls /lustre/cms/store/data/Run2011A/SingleMu/AOD/May10ReReco-v1/`; do
-	for folder2 in `ls /lustre/cms/store/data/Run2011A/SingleMu/AOD/May10ReReco-v1/${folder1}/`; do
+for folder1 in `ls /lustre/cms/store/data/Run2011A/DoubleElectron/AOD/PromptReco-v6/000/`; do
+	for folder2 in `ls /lustre/cms/store/data/Run2011A/DoubleElectron/AOD/PromptReco-v6/000/${folder1}/`; do
+		for file in `ls -l /lustre/cms/store/data/Run2011A/DoubleElectron/AOD/PromptReco-v6/000/${folder1}/${folder2}/ | grep ' ? ' | awk '{print $7}' `; do
 
 		#echo $folder1 $folder2
-		ls -l /lustre/cms/store/data/Run2011A/SingleMu/AOD/May10ReReco-v1/${folder1}/${folder2} | grep ' ? ' #>> out.txt
-
+		echo /lustre/cms/store/data/Run2011A/DoubleElectron/AOD/PromptReco-v6/000/${folder1}/${folder2}/${file}
+		done
 	done
 done
 
-for folder1 in `ls /lustre/cms/store/data/Run2011A/SingleMu/AOD/PromptReco-v4/000/`; do
-	for folder2 in `ls /lustre/cms/store/data/Run2011A/SingleMu/AOD/PromptReco-v4/000/${folder1}/`; do
-
-		#echo ls /lustre/cms/store/data/Run2011A/SingleMu/AOD/PromptReco-v4/000/${folder1}/${folder2}
-		var=$(ls -l /lustre/cms/store/data/Run2011A/SingleMu/AOD/PromptReco-v4/000/${folder1}/${folder2} | grep ' ? ') 
-		if [ "$var" ] 
-			then
-				echo /lustre/cms/store/data/Run2011A/SingleMu/AOD/PromptReco-v4/000/${folder1}/${folder2}/$var #>> out.txt
-		fi
-
-	done
-done
-
-for folder1 in `ls /lustre/cms/store/data/Run2011A/SingleMu/AOD/PromptReco-v6/000/`; do
-	for folder2 in `ls /lustre/cms/store/data/Run2011A/SingleMu/AOD/PromptReco-v6/000/${folder1}/`; do
+for folder1 in `ls /lustre/cms/store/data/Run2011B/DoubleElectron/AOD/PromptReco-v1/000/`; do
+	for folder2 in `ls /lustre/cms/store/data/Run2011B/DoubleElectron/AOD/PromptReco-v1/000/${folder1}/`; do
+		for file in `ls -l /lustre/cms/store/data/Run2011B/DoubleElectron/AOD/PromptReco-v1/000/${folder1}/${folder2}/ | grep ' ? ' | awk '{print $7}' `; do
 
 		#echo $folder1 $folder2
-		var=$(ls -l /lustre/cms/store/data/Run2011A/SingleMu/AOD/PromptReco-v6/000/${folder1}/${folder2} | grep ' ? ')
-		if [ "$var" ] 
-			then
-				echo /lustre/cms/store/data/Run2011A/SingleMu/AOD/PromptReco-v6/000/${folder1}/${folder2}/$var #>> out.txt
-		fi
-
-	done
-done
-
-for folder1 in `ls /lustre/cms/store/data/Run2011B/SingleMu/AOD/PromptReco-v1/000/`; do
-	for folder2 in `ls /lustre/cms/store/data/Run2011B/SingleMu/AOD/PromptReco-v1/000/${folder1}/`; do
-
-		#echo $folder1 $folder2
-		var=$(ls -l /lustre/cms/store/data/Run2011B/SingleMu/AOD/PromptReco-v1/000/${folder1}/${folder2} | grep ' ? ')
-
-		if [ "$var" ] 
-			then
-				echo /lustre/cms/store/data/Run2011B/SingleMu/AOD/PromptReco-v1/000/${folder1}/${folder2}/$var #>> out.txt
-		fi
-
+		echo /lustre/cms/store/data/Run2011B/DoubleElectron/AOD/PromptReco-v1/000/${folder1}/${folder2}/${file}
+		done
 	done
 done
