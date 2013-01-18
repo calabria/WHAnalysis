6f5c501d175f305a001f2153283f1da9e90bb060
Cesare <cesare.calabria23@gmail.com>
2013-01-18 16:28:26 +0100

    Added number of total and filtered events

--- WHAnalysis/SimpleCounter/src/SimpleCounter.cc
+++ WHAnalysis/SimpleCounter/src/SimpleCounter.cc
@@ -24,11 +24,11 @@
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
-
+#include "FWCore/Framework/interface/LuminosityBlock.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
+#include "DataFormats/Common/interface/MergeableCounter.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
@@ -51,11 +51,13 @@ class SimpleCounter : public edm::EDAnalyzer {
 
       virtual void beginRun(edm::Run const&, edm::EventSetup const&);
       virtual void endRun(edm::Run const&, edm::EventSetup const&);
-      virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
-      virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+      virtual void beginLuminosityBlock(const edm::LuminosityBlock &, const edm::EventSetup &);
+      virtual void endLuminosityBlock(const edm::LuminosityBlock &, const edm::EventSetup &);
 
       // ----------member data ---------------------------
 
+      int nEventsTotal;
+      int nEventsFiltered;
       std::map<std::string,TH1F*> histContainer_; 
 };
 
@@ -95,7 +97,8 @@ SimpleCounter::~SimpleCounter()
 void
 SimpleCounter::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
+  
+  using namespace edm;
 
   double count = 1.;
   histContainer_["N_eventi"]->Fill(count);
@@ -108,10 +111,15 @@ void
 SimpleCounter::beginJob()
 {
 
+  nEventsTotal = 0;
+  nEventsFiltered = 0;
+
   // register to the TFileService
   edm::Service<TFileService> fs;
 
   histContainer_["N_eventi"]=fs->make<TH1F>("N_eventi", "count", 2, 0., 2.);
+  histContainer_["N_eventi_Tot"]=fs->make<TH1F>("N_eventi_Tot", "N_eventi_Tot", 2, 0., 2.);
+  histContainer_["N_eventi_Filtered"]=fs->make<TH1F>("N_eventi_Filtered", "N_eventi_Filtered", 2, 0., 2.);
 
 }
 
@@ -119,6 +127,10 @@ SimpleCounter::beginJob()
 void 
 SimpleCounter::endJob() 
 {
+
+  histContainer_["N_eventi_Tot"]->SetBinContent(1,nEventsTotal);
+  histContainer_["N_eventi_Filtered"]->SetBinContent(1,nEventsFiltered);
+
 }
 
 // ------------ method called when starting to processes a run  ------------
@@ -141,8 +153,20 @@ SimpleCounter::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup
 
 // ------------ method called when ending the processing of a luminosity block  ------------
 void 
-SimpleCounter::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+SimpleCounter::endLuminosityBlock(const edm::LuminosityBlock & lumi, const edm::EventSetup & setup)
 {
+
+// Total number of events is the sum of the events in each of these luminosity blocks
+  edm::Handle<edm::MergeableCounter> nEventsTotalCounter;
+  lumi.getByLabel("nEventsTotal", nEventsTotalCounter);
+  nEventsTotal += nEventsTotalCounter->value;
+
+  edm::Handle<edm::MergeableCounter> nEventsFilteredCounter;
+  lumi.getByLabel("nEventsFiltered", nEventsFilteredCounter);
+  nEventsFiltered += nEventsFilteredCounter->value;
+
+  std::cout<<"Eventi "<<nEventsTotal<<" "<<nEventsFiltered<<std::endl;
+
 }
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
